756c10cce91e562be89f651e0214f56eef42ecd7
rai <rai99cos@gmail.com>
2017-04-26 18:38:13 +0900

    add test audio success

--- Shout-it-Aloud/AudioViewController.swift
+++ Shout-it-Aloud/AudioViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 import AudioKit
 
 class AudioViewController: UIViewController {
+    var oscillator = AKOscillator()
+    var oscillator2 = AKOscillator()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        AudioKit.output = AKMixer(oscillator, oscillator2)
+        AudioKit.start()
         // Do any additional setup after loading the view.
     }
 
@@ -25,5 +29,18 @@ class AudioViewController: UIViewController {
     @IBAction func back() {
         self.dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func toggleSound(sender: UIButton) {
+        if oscillator.isPlaying {
+            oscillator.stop()
+            sender.setTitle("Play Sine Wave", for: .normal)
+        } else {
+            oscillator.amplitude = random(0.5, 1)
+            oscillator.frequency = random(220, 880)
+            oscillator.start()
+            sender.setTitle("Stop Sine Wave at \(Int(oscillator.frequency))Hz", for: .normal)
+        }
+    }
+    
 
 }
